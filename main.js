8c65b5cff2ab542a3ee3661d7599877d94542631
hiproenca <higorproenca@gmail.com>
2023-07-10 00:48:27 -0300

    Entender melhor como funcionar innerText para resolver erro

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("first main")
+//Global var
+
+const time_el = document.querySelector('.principal__Clock .principal__Time');
+const stop_btn = document.getElementById('Stop');
+const start_btn = document.getElementById('Start');
+const reset_btn = document.getElementById('Reset');
+
+let seconds = 220;
+let intervals = null;
+
+//Events
+
+//Timer Update
+
+function timer() {
+    seconds++;
+
+    let sec = seconds % 60;
+    let min = Math.floor(seconds / 60);
+    let hr = Math.floor(seconds / 3600);
+
+    time_el = elementinner.HTML = '${ hr } : ${ min } : ${ sec }';
+
+}
+
+timer();
